a4de970ebc47c09967dc1a809d449f623158212d
mimicha90 <meriem.safi90@gmail.com>
2023-02-03 12:21:27 +0100

    rajout de projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,33 +9,33 @@ const regexMail = new RegExp('^[a-zA-Z0-9.-]+[@]{1}[a-zA-Z0-9.-]+[.]{1}[a-z]{2,1
 const regname = new RegExp('^[a-zA-A]');
 const regMessage = new RegExp('^.{1,500}$');
 form.addEventListener("submit", function (e) {
-    let hasError = false;
     e.preventDefault();
     const validateInputs = () => {
         if (namee.value === "" || email.value === "" || message.value === "") {
             if (namee.value === "" || !regname.test(namee.value)) {
-                hasError = true;
                 namee.style.borderColor = "red";
                 nameError.textContent = "veuillez saisir un nom";
             } else {
                 namee.style.borderColor = "green";
+                nameError.textContent = "";
             }
             if (email.value === "" || !regexMail.match(message.value)) {
                 email.style.borderColor = "red";
                 mailError.textContent = "veuillez saisir un email";
             } else {
                 email.style.borderColor = "green";
+                mailError.textContent = "";
             }
             if (message.value === "" || !regMessage.match(message.value)) {
                 message.style.borderColor = "red";
                 messageError.textContent = "veuillez ecire un message";
             } else {
                 message.style.borderColor = "green";
+                messageError.textContent = "";
             }
-        } else {
-            form.submit();
         }
     }
+
     Email.send({
         SecureToken: "d9e5134d-75a4-4a74-9e3d-7be1d038a4bf",
         To: 'meriem.safi90@gmail.com',
@@ -45,6 +45,11 @@ form.addEventListener("submit", function (e) {
     }).then(
         message => alert(message)
     )
+
 })
-validateInputs();
+
+
+
+
+
 
