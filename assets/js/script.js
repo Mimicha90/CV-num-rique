0b990a4507fab804d3ca42317b7423c30ef770c3
mimicha90 <meriem.safi90@gmail.com>
2023-02-03 10:46:36 +0100

    modif

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,46 +10,33 @@ const regname = new RegExp('^[a-zA-A]');
 const regMessage = new RegExp('^.{1,500}$');
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    let hasError = false;
-    //Vérification du nom
-    if ((!namee.value)) {
-        nameError.textContent = "veuillez saisir un nom";
-        namee.focus();
-        hasError = true;
-        console.log(hasError);
-    } else if (!regname.test(namee.value)) {
-        nameError.textContent = "le format du nom est incorrect";
-        namee.focus();
-        hasError = true;
+    // let hasError = false;
+    const validateInputs = () => {
+        if (namee.value === "" || email.value === "" || message.value === "") {
+            if (namee.value === "" || !regname.test(namee.value)) {
+                namee.style.borderColor = "red";
+                nameError.textContent = "veuillez saisir un nom";
+            }
+            if (email.value === "" || !regexMail.match(message.value)) {
+                email.style.borderColor = "red";
+                mailError.textContent = "veuillez saisir un email";
+            }
+            if (message.value === "" || !regMessage.match(message.value)) {
+                message.style.borderColor = "red";
+                messageError.textContent = "veuillez ecire un message";
+            }
+        } else {
+            form.submit();
+        }
     }
-    // Vérification de l'adresse mail
-    if (!email.value) {
-        mailError.textContent = "l'adresse mail est invalide";
-        email.focus();
-        hasError = true;
-    } else if (!regexMail.test(email.value)) {
-        mailError.textContent = "le format de l/email est invalide";
-        email.focus();
-        hasError = true;
-    }
-    if ((!message.value)) {
-        messageError.textContent = "veuillez saisir un message";
-        message.focus();
-        hasError = true;
-    } else if (!regMessage.test(message.value)) {
-        messageError.textContent = "le format du message est incorrect";
-        message.focus();
-        hasError = true;
-    } else {
-        Email.send({
-            SecureToken: "d9e5134d-75a4-4a74-9e3d-7be1d038a4bf",
-            To: 'meriem.safi90@gmail.com',
-            From: "meryoumatou@hotmail.fr",
-            Subject: "mail CV",
-            Body: "De:" + email.value + "\n" + message.value,
-        }).then(
-            message => alert(message)
-        )
-    };
-});
+    Email.send({
+        SecureToken: "d9e5134d-75a4-4a74-9e3d-7be1d038a4bf",
+        To: 'meriem.safi90@gmail.com',
+        From: "meryoumatou@hotmail.fr",
+        Subject: "mail CV",
+        Body: "De:" + email.value + "\n" + message.value,
+    }).then(
+        message => alert(message)
+    )
+})
 
