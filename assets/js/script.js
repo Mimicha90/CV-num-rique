ec28b864a2945aef93af8abf2d368335c224e11b
mimicha90 <meriem.safi90@gmail.com>
2023-02-03 09:57:32 +0100

    modif

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,54 @@ let namee = document.getElementById("name");
 let message = document.getElementById("message");
 let email = document.getElementById("email");
 var submit = document.getElementById('submit');
-function verif() {
-    if (namee.value == "") {
-        alert("veuillez saisir un nom !");
-        namee.focus();
-        return false;
-    }
-    if (email.value == "") {
-        alert("Tapez un email valable pour avoir une réponse.");
-        document.getElementById("email").focus();
-        return false;
-    }
-    if (message.value == "") {
-        alert("Pensez à taper un message !");
-        message.focus();
-        return false;
-    }
-}
-
-
-submit.addEventListener('click', function (e) {
-    verif();
+let nameError = document.getElementById("nameError");
+let mailError = document.getElementById("email");
+let messageError = document.getElementById("message");
+const regexMail = new RegExp('^[a-zA-Z0-9.-]+[@]{1}[a-zA-Z0-9.-]+[.]{1}[a-z]{2,10}$');
+const regname = new RegExp('^[a-zA-A]');
+const regMessage = new RegExp('^.{1,500}$');
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-    Email.send({
-        SecureToken: "d9e5134d-75a4-4a74-9e3d-7be1d038a4bf",
-        To: 'meriem.safi90@gmail.com',
-        From: "meryoumatou@hotmail.fr",
-        Subject: "mail CV",
-        Body: "De:" + email.value + " " + message.value,
-    }).then(
-        message => alert(message)
-    );
+    let hasError = false;
+    //Vérification du nom
+    if ((!namee.value)) {
+        nameError.textContent = "veuillez saisir un nom";
+        namee.focus();
+        hasError = true;
+        console.log(hasError);
+    } else if (!regname.test(namee.value)) {
+        nameError.textContent = "le format du nom est incorrect";
+        namee.focus();
+        hasError = true;
+    }
+    // Vérification de l'adresse mail
+    if (!email.value) {
+        mailError.textContent = "l'adresse mail est invalide";
+        email.focus();
+        hasError = true;
+    } else if (!regexMail.test(email.value)) {
+        mailError.textContent = "le format de l/email est invalide";
+        email.focus();
+        hasError = true;
+    }
+    if ((!message.value)) {
+        messageError.textContent = "veuillez saisir un message";
+        message.focus();
+        hasError = true;
+    } else if (!regMessage.test(message.value)) {
+        messageError.textContent = "le format du message est incorrect";
+        message.focus();
+        hasError = true;
+    } else {
+        Email.send({
+            SecureToken: "d9e5134d-75a4-4a74-9e3d-7be1d038a4bf",
+            To: 'meriem.safi90@gmail.com',
+            From: "meryoumatou@hotmail.fr",
+            Subject: "mail CV",
+            Body: "De:" + email.value + "\n" + message.value,
+        }).then(
+            message => alert(message)
+        )
+    };
 });
 
